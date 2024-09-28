12277837e6e0ec9697d262ceb05fa81424ef0d76
Roman Nifontov <romannif@mail.ru>
2024-09-28 16:19:20 +0500

    Добавил ArrayList в проект

--- Faang_school/Java/App.java
+++ Faang_school/Java/App.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Магазин открывается через 5 дней");
         System.out.println("Welcome to Java");
-        for (int i = 0; i < 10 ; i++) {
-            System.out.println(i);
-        }
+        ArrayList<Integer> test = new ArrayList<>(Arrays.asList(1,2,34,5,543,453));
+        System.out.println(test);
+
     }
 
 }
