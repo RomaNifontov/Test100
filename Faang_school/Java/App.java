aad9dc20aaa20cfd41f76643da1919843a600ef5
Roman Nifontov <romannif@mail.ru>
2024-09-28 16:18:10 +0500

    Добавил счетчик до 10 в Test3

--- Faang_school/Java/App.java
+++ Faang_school/Java/App.java
@@ -2,6 +2,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Магазин открывается через 5 дней");
         System.out.println("Welcome to Java");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(i);
+        }
     }
 
 }
