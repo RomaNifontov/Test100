4cb4f04cb67803e7991eaf7d8727965bf998f6c5
Roman Nifontov <romannif@mail.ru>
2024-09-28 15:47:28 +0500

    Добавил ветку feature_b, исправил количество дней

--- Faang_school/Java/App.java
+++ Faang_school/Java/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Магазин открывается через 5 дней");
+        System.out.println("Магазин открывается через 10 дней");
+        System.out.println("Welcome to Java");
+
     }
 
 }
