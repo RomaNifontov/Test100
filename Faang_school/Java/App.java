e6637fa14e945d72b4a018f625078c4d9da5a243
Roman Nifontov <romannif@mail.ru>
2024-09-28 15:40:26 +0500

    Добавил приветствие в Task2

--- Faang_school/Java/App.java
+++ Faang_school/Java/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Магазин открывается через 5 дней");
+        System.out.println("Добро пожаловать");
     }
 
 }
